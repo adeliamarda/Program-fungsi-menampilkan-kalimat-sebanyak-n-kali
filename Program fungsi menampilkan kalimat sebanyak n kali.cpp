c63d84bd1db952b120ebfb90f403cfbf2cb72081
Adelia Marda Trisno <46318729+adeliamarda@users.noreply.github.com>
2019-01-03 19:32:04 +0700

    Add files via upload

---
+++ Program fungsi menampilkan kalimat sebanyak n kali.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+int i,n;
+cout<<"Menampilkan kalimat Algoritma dan Pemograman sebanyak n kali"<<endl;
+cout<<"Masukan jumlah yang diinginkan : ";
+cin>>n;
+
+for(i=1; i<=n; ++i)
+{
+    cout<<"Algoritma dan Pemograman"<<endl;
+}
+return 0;
+}
